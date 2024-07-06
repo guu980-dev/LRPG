0ac0e59a29ba3df0a925ab28c745b3f226e58f7a
jacob <jacob@enfit.net>
2024-07-06 14:48:43 +0900

    update select_option by jacob

--- demo/select_option.py
+++ demo/select_option.py
@@ -2,6 +2,7 @@ import gradio as gr
 import random
 
 var1 = [1, 2, 3]
+
 def track_score(var1):
     return var1
 
@@ -11,17 +12,24 @@ def scenario():
 
 var2 = scenario()
 
+# Initial title and description
+initial_title = "당신의 선택은? "
+initial_description = "<div style='display: flex; justify-content: center; align-items: center; height: auto;'><img src='https://i.ytimg.com/vi/Reo3MnaVb5M/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD6ZP2I8d1yVXB9Aet0fv1qCeZmNQ' alt='annotated'></div>"
+
 def select_option(option):
+    global var1
+    var1 = [4, 5, 6]
     return var2, "https://dummyimage.com/300/09f.png"
 
 scores = track_score(var1)
 
 demo = gr.Interface(
     fn=select_option,
-    inputs=gr.Radio(scores),
+    inputs=gr.Radio(var1),
     outputs=["text", "image"],
-    title="당신의 선택은? ",
-    description="<div style='display: flex; justify-content: center; align-items: center; height: auto;'><img src='https://i.ytimg.com/vi/Reo3MnaVb5M/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD6ZP2I8d1yVXB9Aet0fv1qCeZmNQ' alt='annotated'></div>",
+    title=initial_title,
+    description=initial_description,
+    live=True  # live 업데이트를 활성화
 )
 
 demo.launch()
